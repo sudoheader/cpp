7e03879e708c5b316f55a8acb5f77477028c0a2f
sudoheader <sudoheader@users.noreply.github.com>
2017-09-04 19:25:24 -0700

    125. Calling methods and functions

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,16 @@
 //clang 3.8.0
 
 #include <iostream>
+using namespace std;
+
+int secondMain()
+{
+    cout << "In secondMain!\n";
+}
 
 int main()
 {
-    std::cout << "Hello, world!\n";
+    secondMain();
+    cout << "Hello, world!\n";
+    secondMain();
 }
