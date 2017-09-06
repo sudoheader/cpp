cbf22d85ec00e3932da09cab006ab2eb402f6588
sudoheader <sudoheader@users.noreply.github.com>
2017-09-05 21:17:20 -0700

    140. For statements

--- hello.cpp
+++ hello.cpp
@@ -3,33 +3,8 @@
 #include <iostream>
 using namespace std;
 
-int GlobalA = 9;
-
-void secondMain(int aSecondMain)
-{
-    cout << "In secondMain! " << aSecondMain << "\n";
-}
-
 int main()
 {
-    int aMain = 1;
-    int bMain = 9;
-    if(aMain == 3)
-    {
-        aMain = aMain + 1;
-    }
-    else if((aMain > 3) && (aMain < 8))
-    {
-        aMain = aMain - 1;
-    }
-    else if(aMain > 8)
-    {
-        aMain = aMain + 10;
-    }
-    else
-    {
-        aMain = 2;
-    }
-    cout << "Score: " << aMain << "\n";
-    // secondMain(aMain);
+    for(int i = 1; i < 6; i++)
+        cout << "This is: " << i << "\n";
 }
