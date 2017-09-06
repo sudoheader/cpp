752c05a456319d816f65bf5eb3a8e43b36812e45
sudoheader <sudoheader@users.noreply.github.com>
2017-09-05 21:09:31 -0700

    >136. Operators

--- hello.cpp
+++ hello.cpp
@@ -13,9 +13,9 @@ void secondMain(int aSecondMain)
 int main()
 {
     int aMain = 3;
-    if(aMain == 2)
+    if(aMain != 2)
     {
-        aMain = aMain - 1;
+        aMain = aMain + 1;
     }
     cout << "Score: " << aMain << "\n";
     // secondMain(aMain);
