9cc32f7c54d7fd4465946f5e4334774d3abe0cd2
sudoheader <sudoheader@users.noreply.github.com>
2017-09-05 21:07:53 -0700

    135. If statments

--- hello.cpp
+++ hello.cpp
@@ -13,6 +13,10 @@ void secondMain(int aSecondMain)
 int main()
 {
     int aMain = 3;
+    if(aMain == 2)
+    {
+        aMain = aMain - 1;
+    }
     cout << "Score: " << aMain << "\n";
-    secondMain(aMain);
+    // secondMain(aMain);
 }
